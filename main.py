f085dfc6c5bb3e674e931e030af59ad1fcdadad3
Zack Crenshaw <zcrenshaw@cs.uchicago.edu>
2019-07-16 15:51:43 -0400

    to be deleted

--- main.py
+++ main.py
@@ -1,48 +1,24 @@
-try:
-    from urllib.request import urlopen
-except ImportError:
-	from urllib2 import urlopen
+#Zack Crenshaw
 
-import csv
+#Slang Transmission Project
+#run analysis given certain parameters
 
-COUNT = 4;
-SUBREDDIT = 5;
+# python3 main.py (subreddit) (search term) (longest time ago) (interval of time)
 
-def getpage(url):
-	page = urlopen(url)
-	html = page.read()
-	return html
+import sys
 
-def splitpage(page):
-	return page.split("\n")
-
-def getfield(lines, field):
-	try:
-		returnme = lines[field].split(":")[1].strip("\t ,[]")
-	except IndexError:
-		return ""
-	return returnme.strip('"')
-
-#this code just tracks total usage, not number of users
 def main():
-	terms = ['damn','shit']
-	subs = ["hockey"]
-	page_link = "http://api.pushshift.io/reddit/search/submission/?q=hello&aggs=subreddit&subreddit=askscience&after=5d"
-	basic = ["http://api.pushshift.io/reddit/search/submission/?q=","&aggs=subreddit&subreddit=","&after=","d&before=","d"]
-	fieldnames = ['Count','Subreddit']
-	
-	with open('output.csv', mode='w') as out:
-		writer = csv.DictWriter(out, fieldnames=fieldnames)
-		writer.writeheader()
-		for i in range(10,100,10):
-			query = basic[0]+terms[0]+basic[1]+subs[0]+basic[2]+str(i)+basic[3]+str(i-10)+basic[4]
-			print(query)
-			page = splitpage(getpage(query))
-			count = getfield(page,COUNT)
-			if count=="":
-				count = 0;
-			sub = subs[0]
-			print(sub + '\t' + terms[0] + '\t' + str(count))	
-			writer.writerow({fieldnames[0]: count, fieldnames[1]: sub})
 
-main()
+    subreddit = sys.argv[1]
+    slang = sys.argv[2]
+    longest = sys.argv[3]
+    interval = sys.argv[4]
+
+    search = ""
+
+
+    return
+
+
+if __name__ == '__main__':
+    main()
---
+++ webScrape.py
@@ -0,0 +1,19 @@
+#Zack Crenshaw
+
+#Slang Transmission Project
+#Scrape data from the web, output to CSV
+
+#python3 webScrape.py (URL)
+
+import csv,requests,sys
+from bs4 import BeautifulSoup
+
+def main():
+
+	url = sys.argv[1]
+
+	return
+
+
+if __name__ == '__main__':
+	main()
