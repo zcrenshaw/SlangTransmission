d354ad3e1a0b83b1bbd8d9e2facc904e025ca424
Zack Crenshaw <zcrenshaw@cs.uchicago.edu>
2019-09-19 15:14:07 -0500

    v5 CSVmanip

--- scripts/CSVmanip.py
+++ scripts/CSVmanip.py
@@ -11,6 +11,8 @@ import re
 
 
 def prep_df(filename,droplink):
+    # prepares a data frame for more processing
+    # prunes some metadata away
     try:
         df = pd.read_csv(filename)
         if droplink:
@@ -30,17 +32,20 @@ def prep_df(filename,droplink):
 
 
 def rolling_average(df,smoothing):
+    # computes a rolling average over a data set
     df['avg'] = pd.Series([])
     df['avg'] = df['count'].rolling(window=smoothing, center=True).mean()
     return df
 
 def append_data(df1,name1,df2,name2):
+    # appends data together
     # df1 input
     # df2 output
     df2[name2] = df1[name1]
     return df2
 
 def del_col(df,col):
+    # deletes a column
     try:
         df = df.drop(col,axis=1)
     except:
@@ -49,6 +54,7 @@ def del_col(df,col):
 
 
 def count_influence(q,src,dst):
+    # counts the number of influencers that contain term q
     after = []
     before = []
     sums = []
@@ -71,16 +77,57 @@ def count_influence(q,src,dst):
 
     df.to_csv(dst,index=False)
 
-def arith(src,cols,val):
+def arith(src,cols,val,dst):
     # computes arithmetic on columns of given file
     df = pd.read_csv(src)
     for c in cols:
-        df[c] += val
-    df.to_csv(src)
-    return
+        try:
+            df[c] += val
+        except:
+            print("Column not present, but resaving file in new destination")
+            break
+    df.to_csv(dst)
+
+def make_adjustment(src,cols,mod):
+    # rounds timescale to number divisble by mod
+    # rounds down
+    # does adjustment on one file
+    val = int(src.split('_')[-2][:-1])
+    delta = val % mod
+    if delta != 0:
+        # if change is needed
+        delta *= -1
+        dst = adjust_dst(src,delta)
+        arith(src, cols,delta,dst)
+
+def adjust_dst(src,delta):
+    # creates new path based on the delta
+    editpath = src.split('_') # isolate parameters
+    # adjust numbers
+    editpath[-2] = str(int(editpath[-2][:-1]) + delta) + editpath[-2][-1]
+    editpath[-3] = str(int(editpath[-3][:-1]) + delta) + editpath[-3][-1]
+    # rejoin string name
+    editpath[-2] = ''.join(editpath[-2])
+    editpath[-3] = ''.join(editpath[-3])
+    dst = '_'.join(editpath)
+    return dst
+
+
+def adjust_timescale(src,cols,mod):
+    # rounds timescale to number divisble by mod
+    # does adjustment to entire folder
+    if src[-3:] == 'csv':
+        make_adjustment(src,cols,mod)
+    elif os.path.isdir(src):
+        for file in os.listdir(src):
+            adjust_timescale(src+file,cols,mod)
+    else:
+        print(src[-3:])
+        print("Error: file must be .csv format – did not execute")
 
 
 def manip():
+    # main function, executes the desired manipulations
     func = sys.argv[1]
 
     if func == 'roll-avg':
@@ -135,6 +182,7 @@ def manip():
             outdf = append_data(prep_df(filepath,True),incol,outdf,outcol)
             outdf.to_csv(outpath[:-4]+"_ALL.csv")
     elif func == "append":
+        # append files together, output to oufile
         file1 = pd.read_csv(sys.argv[2])
         file2 = pd.read_csv(sys.argv[3])
         outfile = sys.argv[4]
@@ -143,16 +191,26 @@ def manip():
         output.to_csv(outfile)
 
     elif func == "count-influence":
+        # count the occurence of a term in an influencer file
         q = sys.argv[2]
         folder = sys.argv[3]
         output = sys.argv[4]
         count_influence(q,folder,output)
 
     elif func == "arith":
-        src = sys.argv[1]
-        cols = sys.argv[2]
-        val = sys.argv[3]
-        arith(src,cols,val)
+        # adds val to a column
+        src = sys.argv[2]
+        cols = sys.argv[3]
+        val = sys.argv[4]
+        arith(src,cols,val,src)
+    elif func == "timescale":
+        # adjusts timescale on a file or folder
+        # cols should be "after" and "before" for timescale adjustment
+        # file path name only works for timescale adjustment
+        src = sys.argv[2]
+        cols = sys.argv[3].split(',')
+        mod = int(sys.argv[4])
+        adjust_timescale(src,cols,mod)
 
     else:
         print("Not yet supported")
